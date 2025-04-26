69e837ed1f4d10dc398b941544be68adb65ecda0
BlueNebulaa <nbil.ibrahim04@gmail.com>
2025-04-27 06:52:37 +0700

    sentiment analysis

--- app.py
+++ app.py
@@ -6,13 +6,21 @@ import joblib
 from nltk.tokenize import word_tokenize
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+import re
+import string
+import unicodedata
+import emoji
+import pandas as pd
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 
 
 app = Flask(__name__)
 CORS(app)
 
-model = joblib.load('Backend Python/model_multinomial_nb.pkl')
+model = joblib.load('model_multinomial_nb.pkl')
 
 
 def prep(tweet):
@@ -37,65 +45,26 @@ def prep(tweet):
 
     tweet = tweet.replace("#", "").replace("_", " ")
     return tweet
+  
+def predict_sentiment(text_input):
+    # Load model dan vectorizer
+    model = joblib.load('model_multinomial_nb.pkl')
+    vectorizer = joblib.load('vectorizer.pkl')
+
+    # Transformasi teks input menggunakan vectorizer
+    text_tfidf = vectorizer.transform([text_input]).toarray()
+
+    # Prediksi menggunakan model
+    prediction = model.predict(text_tfidf)
 
 
-def slang_to_formal(text, slang_dict):
-    text = str(text) 
-    words = text.split()
-    formal_words = [slang_dict.get(word, word) for word in words]
-    return " ".join(formal_words)
-
-
-def load_slang_dict_from_csv(csv_file):
-    df = pd.read_csv(csv_file,encoding='latin-1')
-
-    if 'anakjakartaasikasik' in df.columns and 'anak jakarta asyik asyik' in df.columns:
-        slang_dict = dict(zip(df['anakjakartaasikasik'], df['anak jakarta asyik asyik']))
-        return slang_dict
-    else:
-        raise ValueError("Kolom 'aamiin' dan 'amin' tidak ditemukan dalam file CSV")
+    return prediction[0]
     
-    
-def tokenize(text):
-    text = word_tokenize(text)
-    return text
-
-
-factory = StopWordRemoverFactory()
-stopword_remover = factory.create_stop_word_remover()
-
-
-def remove_stopwords(text):
-    if isinstance(text, list):  
-        text = " ".join(text)
-    elif not isinstance(text, str):  
-        return ""
-    text = stopword_remover.remove(text)  
-    return text.split()  
-
-
-factory = StemmerFactory()
-
-stemmer = factory.create_stemmer()
-
-def stem_text(text):
-    stemmed_tokens = [stemmer.stem(token) for token in text]
-    return ' '.join(stemmed_tokens)
-
 @app.route('/api/predict', methods=['POST'])    
 def create_prediction():
     data = request.get_json()
     data = prep(data)
-    slang_dict = load_slang_dict_from_csv('/Hackathon/dataset/new_kamusalay.csv')
-    data = slang_to_formal(data, slang_dict)
-    data = tokenize(data)
-    data = remove_stopwords(data)
-    data = stem_text(data)
-    
-    vectorizer = joblib.load("vectorizer.pkl")
-    data = vectorizer.transform([data]).toarray()
-    
-    prediction = model.predict(data)
+    prediction = predict_sentiment(data)
     
     if prediction == 0 :
         prediction = "Negatif"
@@ -103,12 +72,11 @@ def create_prediction():
         prediction = "Positif"
     
     prediction_id = str(uuid.uuid4())
-    prediction_results[prediction_id] = prediction
     response = {
         'id' : prediction_id,
         'prediction' : prediction
     }
-    return jsonify(response), 201
+    return response
 
 
 if __name__ == '__main__':
